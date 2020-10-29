6094203ce96b7e396964093110e293a9ea282a70
ryanl <rleung1004@gmail.com>
2020-10-28 23:49:52 -0700

    Initial commit with movement and look around functionality

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField]
+    private Transform playerRoot, lookRoot;
+
+    [SerializeField]
+    private bool invert;
+
+    [SerializeField]
+    private bool canUnlock = true;
+
+    [SerializeField]
+    private float sensitivity = 5f;
+
+    [SerializeField]
+    private int smoothSteps = 10;
+
+    [SerializeField]
+    private float smoothWeight = 0.4f;
+
+    [SerializeField]
+    private float rollAngle = 10f;
+
+    [SerializeField]
+    private float rollSpeed = 3f;
+
+    [SerializeField]
+    private Vector2 defaultLookLimits = new Vector2(-70f, 80f);
+
+    private Vector2 lookAngles;
+    private Vector2 currentMouseLook;
+    private Vector2 smoothMove;
+    private float currentRollAngle;
+    private int lastLookFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LockAndUnlockCursor();
+
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            LookAround();
+        }
+    }
+
+    void LockAndUnlockCursor()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                Cursor.lockState = CursorLockMode.None;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+
+    void LookAround()
+    {
+        currentMouseLook = new Vector2(Input.GetAxis(MouseAxis.MOUSE_Y), Input.GetAxis(MouseAxis.MOUSE_X));
+
+        lookAngles.x += currentMouseLook.x * sensitivity * (invert ? 1f : -1f);
+        lookAngles.y += currentMouseLook.y * sensitivity;
+
+        lookAngles.x = Mathf.Clamp(lookAngles.x, defaultLookLimits.x, defaultLookLimits.y);
+
+        currentRollAngle = Mathf.Lerp(currentRollAngle, Input.GetAxisRaw(MouseAxis.MOUSE_X) * rollAngle, Time.deltaTime * rollSpeed);
+
+        lookRoot.localRotation = Quaternion.Euler(lookAngles.x, 0f, currentRollAngle);
+        playerRoot.localRotation = Quaternion.Euler(0f, lookAngles.y, 0f);
+    }
+}
---
+++ Assets/Scripts/Player/PlayerSprintAndCrouch.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSprintAndCrouch : MonoBehaviour
+{
+    private PlayerMovement playerMovement;
+
+    public float sprintSpeed = 10f;
+    public float moveSpeed = 5f;
+    public float crouchSpeed = 2f;
+
+    private Transform lookRoot;
+    private float standHeight = 1.6f;
+    private float crouchHeight = 1f;
+
+    private bool isCrouching;
+    
+    void Awake()
+    {
+        playerMovement = GetComponent<PlayerMovement>();
+
+        lookRoot = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Sprint();
+        Crouch();
+    }
+
+    void Sprint()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && !isCrouching)
+        {
+            playerMovement.speed = sprintSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift) && !isCrouching)
+        {
+            playerMovement.speed = moveSpeed;
+        }
+    }
+
+    void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            if (isCrouching)
+            {
+                lookRoot.localPosition = new Vector3(0f, standHeight, 0f);
+                playerMovement.speed = moveSpeed;
+                isCrouching = false;
+            }
+            else
+            {
+                lookRoot.localPosition = new Vector3(0f, crouchHeight, 0f);
+                playerMovement.speed = crouchSpeed;
+                isCrouching = true;
+            }
+           
+        }
+    }
+}
